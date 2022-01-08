458f613831bc13c6919a9030d67599188845d351
silas2009 <97012583+silas2009@users.noreply.github.com>
2022-01-07 21:36:12 -0600

    Create Commands.lua

---
+++ Server/ModuleScripts/Commands.lua
@@ -0,0 +1,18 @@
+local Settings = _G.Settings
+
+-- Commands
+
+local commands = {
+	{
+		Name = "kill",
+		Nicknames = {"die"},
+		Command = function()
+            
+		end,
+		UnCommand = function()
+
+		end,
+	},
+}
+
+return commands
